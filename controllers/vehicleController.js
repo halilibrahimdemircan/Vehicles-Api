3aef854d81b64a167e681f34dff4cdf71451f5bc
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2022-03-25 15:25:58 +0300

    vehicle controller

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,7 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
-const { vehicledb } = require('../models/db')
-const { Car } = require('../models');
+const { Car, Bus, Boat } = require('../models');
 
 
 
@@ -24,7 +23,28 @@ exports.getVehiclesByColor = catchAsync(async (req, res, next) => {
         });
     } else if (vehicle_type === "bus") {
 
+        const buses = await Bus.findAll({
+            where: {
+                color: color
+            }
+        })
+        res.status(200).send({
+            status: 'success',
+            data: buses,
+            text: 'Successfully fetched buses'
+        })
+
     } else if (vehicle_type === "boat") {
+        const boats = await Boat.findAll({
+            where: {
+                color: color
+            }
+        })
+        res.status(200).send({
+            status: 'success',
+            data: boats,
+            text: 'Successfully fetched boats'
+        })
 
     } else {
         return next(new AppError(400, 'vehicle_type must be car, bus or boat', "invalid input"));
@@ -32,66 +52,143 @@ exports.getVehiclesByColor = catchAsync(async (req, res, next) => {
 });
 
 exports.updateVehicleById = catchAsync(async (req, res, next) => {
+
+
+    const vehicle_type = req.params.vehicle_type;
     const id = req.params.id;
     const headlight = req.body.headlight;
     const wheel = req.body.wheel;
     const color = req.body.color;
 
+
+
     if (!(headlight == "on" || headlight == "off")) {
         return next(new AppError(400, 'headlight must be on or off', "invalid input"));
 
     }
-    const car = await Car.update({
 
-        headlight: headlight == "on" ? true : false,
-        wheel: wheel,
-        color: color
-    },
-        {
-            where: { id: id },
-            returning: true
-        })
+    if (vehicle_type === "car") {
+        const car = await Car.update({
 
-    res.status(200).send({
-        status: 'success',
-        data: car,
-    });
+            headlight: headlight == "on" ? true : false,
+            wheel: wheel,
+            color: color
+        },
+            {
+                where: { id: id },
+                returning: true
+            })
+
+        res.status(200).send({
+            status: 'success',
+            data: car,
+        });
+    } else if (vehicle_type === "bus") {
+
+        const bus = await Bus.update({
+            color: color
+        },
+            {
+                where: { id: id },
+                returning: true
+            })
+
+        res.status(200).send({
+            status: 'success',
+            data: bus,
+        });
+    } else if (vehicle_type === "boat") {
+
+        const boat = await Boat.update({
+            color: color
+        },
+            {
+                where: { id: id },
+                returning: true
+            })
+
+        res.status(200).send({
+            status: 'success',
+            data: boat,
+        });
+    } else {
+        return next(new AppError(400, 'vehicle_type must be car, bus or boat', "invalid input"));
+    }
+
+
 });
 
 exports.deleteVehicleById = catchAsync(async (req, res, next) => {
+    const vehicle_type = req.params.vehicle_type;
     const id = req.params.id;
 
-    const car = await Car.destroy({ where: { id: id } });
+    if (vehicle_type === "car") {
+        const car = await Car.destroy({ where: { id: id } });
 
-    res.status(204).send({
-        status: 'success',
-        data: null,
-    });
+        res.status(204).send({
+            status: 'success',
+            data: null,
+        });
+    } else if (vehicle_type === "bus") {
+        const bus = await Bus.destroy({ where: { id: id } });
+
+        res.status(204).send({
+            status: 'success',
+            data: null,
+        });
+    } else if (vehicle_type === "boat") {
+        const boat = await Boat.destroy({ where: { id: id } });
+
+        res.status(204).send({
+            status: 'success',
+            data: null,
+        });
+    } else {
+        return next(new AppError(400, 'vehicle_type must be car, bus or boat', "invalid input"));
+    }
+
+
 });
 
 exports.createVehicle = catchAsync(async (req, res, next) => {
     const color = req.body.color;
     const headlight = req.body.headlight;
     const wheel = req.body.wheel;
-    // const data = {
-    // }
-
-    // if (wheel !== undefined) {
-    //     data[wheel] = wheel
-    // }
+    const vehicle_type = req.params.vehicle_type;
 
     if (!(headlight == "on" || headlight == "off" || headlight == undefined)) {
         return next(new AppError(400, 'headlight must be on or off', "invalid input"));
 
     }
-    const car = await Car.create({
-        color: color,
-        headlight: headlight == "on" ? true : false,
-        wheel: wheel
-    })
-    res.status(200).send({
-        status: 'success',
-        data: car,
-    });
+
+    if (vehicle_type === "car") {
+        const car = await Car.create({
+            color: color,
+            headlight: headlight == "on" ? true : false,
+            wheel: wheel
+        })
+        res.status(200).send({
+            status: 'success',
+            data: car,
+        });
+    } else if (vehicle_type === "bus") {
+        const bus = await Bus.create({
+            color: color
+        })
+        res.status(200).send({
+            status: 'success',
+            data: bus,
+        });
+    } else if (vehicle_type === "boat") {
+        const boat = await Boat.create({
+            color: color
+        })
+        res.status(200).send({
+            status: 'success',
+            data: boat,
+        });
+    } else {
+        return next(new AppError(400, 'vehicle_type must be car, bus or boat', "invalid input"));
+    }
 }
 );
